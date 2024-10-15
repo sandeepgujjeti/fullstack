1aaedf2da6b72098af862a74592c11c9b861689a
sandeepgujjeti <bittusandeep074@gmail.com>
2024-10-15 14:35:41 +0530

    new files are added

---
+++ react/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test(Props){
+    return(
+        <div>
+            Hello {Props.name} your working as {Props.role}
+            your{Props.salary}per month
+        </div>
+    )
+}
+export default Test;
